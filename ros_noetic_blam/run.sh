f85b67983403f9298f65a2e9ff4488a04cd3fe8f
masachika-kamada <masachika.kmd@gmail.com>
2022-12-09 14:12:28 +0900

    first commit

---
+++ ros_noetic_blam/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+xhost +local:
+
+if [ ! -d "workspace" ]; then
+    mkdir workspace
+fi
+
+docker run -it \
+    -e DISPLAY=unix${DISPLAY} \
+    -e QT_X11_NO_MITSHM=1 \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v $HOME/.Xauthority:/home/ubuntu/.Xauthority \
+    -v $PWD/workspace:/home/ubuntu/catkin_ws \
+    --name=ros-noetic-blam \
+    ros-noetic-blam \
+    bash
